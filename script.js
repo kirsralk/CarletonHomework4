ab64f741de43f763456d6722e073026aa9948151
Kirsi Ralko <kirsiralko@gmail.com>
2020-12-04 21:45:40 -0600

    funcitonal timer on clicking start button

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-<script>
-
-
-</script>
+function startTimer(){
+    var counter = 75;
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = counter;
+      }
+      if (counter === 0) {
+          alert("Sorry, out of time!");
+          clearInterval(counter);
+      }
+    }, 1000);
+  }
+  function start()
+  {
+      document.getElementById("count").style="color:green;";
+      startTimer();
+  };
