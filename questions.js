7b595f3e2482f1733422ac5f52e8302dcd10104b
Kirsi Ralko <kirsiralko@gmail.com>
2020-12-04 21:37:27 -0600

    added questions.js to contain questions to show/hide on index

---
+++ questions.js
@@ -0,0 +1,36 @@
+// Array for questions and answers to show/hide
+var questions = [
+    {
+      Question: "Commonly used data types DO NOT include:",
+      Choices: ["strings", "booleans", "alerts", "numbers"],
+      Answer: "alerts"
+    },
+    {
+      Question: "The condition in an if / else statement is enclosed within ____.",
+      Choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      Answer: "parentheses"
+    },
+    {
+      Question: "Arrays in JavaScript can be used to store ____.",
+      Choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      Cnswer: "all of the above"
+    },
+    {
+      Question:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      Choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      Answer: "quotes"
+    },
+    {
+      Question:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      Choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      Answer: "console.log"
+    }
+  ];
+  
